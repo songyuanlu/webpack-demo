b5fcf180e2f68b89397c1811f90dd0a11e067b9f
songyuanlu <1574994102@qq.com>
2018-10-23 07:33:06 +0800

    TEST

--- webpack.common.js
+++ webpack.common.js
@@ -4,39 +4,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    page1: ['./src/page1/index.js'],
-    index: './src/index.js',
-    another: './src/another-module.js',
-    vendor: './src/common.js'
+    main: './src/main.js'
+    // page1: ['./src/page1/index.js'],
+    // index: './src/index.js',
+    // another: './src/another-module.js',
+    // vendor: './src/common.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-        template: 'index.html',
-        filename: 'index.html'
-      }),
-      new HtmlWebpackPlugin({
-        template: 'index.html',
-        filename: 'song.html'
-      }),
-      new HtmlWebpackPlugin({
-        template: 'home.html',
-        filename: 'home.html'
-      }),
-      new HtmlWebpackPlugin({
-        template: 'src/page1/index.html',
-        filename: 'page1/page1.html'
-      })
+    new CleanWebpackPlugin(['dist'])
+    ,new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index-vue.html'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
       }
     ]
   }
